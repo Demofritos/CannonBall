cc6d3d1ddac30c67f68f0e74e2f4d486e3d44320
Demofritos <linser.seth@stu.jamesirwin.org>
2026-04-02 11:32:52 -0600

    asdfghjkl

--- player.gd
+++ player.gd
@@ -2,7 +2,11 @@ extends CharacterBody2D
 
 const GRAVITY : int = 1000
 const MAX_VEL : int =600
-func _process(delta: float) -> void:
-	if Input.is_action_pressed("jump_up"):
-		position += Vector2(0, -100) * delta
-var gravity = 100 
+var flying : bool = false
+var falling : bool = false
+const START_POS = Vector2(100, 400)
+
+func _ready():
+	falling = false
+	flying = false
+	
